8c2ed690f5a37a030a652c62cdcebda7cd280bbb
ulster02 <torresc515@gmail.com>
2025-02-11 23:42:31 -0600

    Changed Database to Mysql Server

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -86,9 +86,13 @@ WSGI_APPLICATION = 'inventory_management.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            "read_default_file": "D:\Escritorio\inventory-management-sys-backend\env.cnf",
+            'charset': "utf8mb4",
+        },
     }
 }
 
